93ee20d99ddfb2df0a0fca6e56448e472d637c6f
xiyeeeee <claudiachenxiye@gmail.com>
2023-04-12 22:30:05 +0800

    add nav bar to profile, delete acc and change password page

--- MSPcode/change_password.php
+++ MSPcode/change_password.php
@@ -13,6 +13,43 @@
 <body id="profilebg">
 <button id="back-to-top-btn"><i class="fas fa-angle-double-up"></i></button>
     <article>
+    <div class="header-container">
+  <div class="logo-container">
+    <div class="logo">
+      <h1 class="logo-text">Expert<span class="trademark">&reg;</span></h1>
+    </div>
+    <nav class="navbar">
+      <ul>
+        <li class="dropdown">
+          <a href="#">Training <i class="fas fa-bicycle"></i></a>
+          <div class="dropdown-content">
+            <a href="#">Menu</a>
+            <a href="#">Request</a>
+          </div>
+        </li>
+        <li><a href="#">Booking <i class="fas fa-cart-plus"></i></a></li>
+        <li><a href="#">Profile <i class="fas fa-user"></i></a></li>
+        <li><a href="dashboard.php">Home <i class="fas fa-home"></i></a></li>
+        <li class="dropdown">
+          <a href="#">Category <i class="fas fa-bars"></i></a>
+          <div class="dropdown-content">
+            <a href="#">Category 1</a>
+            <a href="#">Category 2</a>
+            <a href="#">Category 3</a>
+            <a href="#">Category 4</a>
+          </div>
+        </li>
+        <li><a href="login.php">Logout <i class="fas fa-user-circle"></i></a></li>
+      </ul>
+    </nav>
+  </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+
         <div class="manage_btn_menu">
             <button onclick="location.href='profile.php'" class="non_active_user">Profile Info</button>
             <button onclick="location.href='change_password.php'" class="active_user">Change Password</button>
