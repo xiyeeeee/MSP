783b8d72c2e38ceb6e28741e5239c3c6c58ae661
xiyeeeee <claudiachenxiye@gmail.com>
2023-04-23 01:03:19 +0800

    add_training front end

--- MSPcode/add_training.php
+++ MSPcode/add_training.php
@@ -3,11 +3,28 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="script.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 	<title>TRAINING: ETMP</title>
 </head>
-<body>
-    <?php
+<body id="add_trainingbg">
+<div class= "dashboard-container">
+  <div class="header-container">
+  <div class="logo-container">
+    <div class="logo">
+      <h1 class="logo-text">Expert<span class="trademark">&reg;</span></h1>
+    </div>
+    <?php include 'adminNav.php';?>
+  </div>
+  </div>
+  </div>
+	<br></br>
+    <br></br>
+    <br></br>
+
+    
+	
+	<?php
+    
     require_once "includes/connect.php";
 
     // Check if image file is a actual image or fake image
@@ -16,7 +33,6 @@
         $target_file = $target_dir . $_POST["tName"] . ".png";
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
         if(isset($_FILES["fileToUpload"]["tmp_name"])){
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
         if($check !== false) {
@@ -26,17 +42,15 @@
             echo "File is not an image.";
             $uploadOk = 0;
         }
-
         if (file_exists($target_file)) {
             echo "Sorry, training already exists.";
             $uploadOk = 0;
         }
-
+        
         if ((!isset($_POST["tName"]) && !isset($_POST["tCategory"]) && !isset($_POST["tLocation"]) && !isset($_POST["tDescription"]) && !isset($_POST["tPrice"]))){
             echo "Please fill in all fields";
             $uploadOk = 0;
         }
-
         if ($uploadOk == 0) {
             echo "Sorry, your file was not uploaded.";
           // if everything is ok, try to upload file
@@ -49,10 +63,14 @@
 
             if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
               echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-              $sql = "INSERT IGNORE INTO trainings (tName, tCategory, tLocation, tDescription, tPrice)
-                    VALUES ('$tName', '$tCategory', '$tLocation', '$tDescription', '$tPrice')";
+              $sql = "INSERT INTO trainings (tName, tCategory, tLocation, tDescription, tPrice) 
+                    SELECT * FROM (SELECT '$tName', '$tCategory', '$tLocation', '$tDescription', '$tPrice') AS tmp
+                    WHERE NOT EXISTS(  
+                    SELECT tName FROM trainings WHERE tName = '$tName') LIMIT 1";  
+              $sql = "INSERT IGNORE INTO trainings (tName, tCategory, tLocation, tDescription, tPrice) 
+                    VALUES ('$tName', '$tCategory', '$tLocation', '$tDescription', '$tPrice')"; 
 
-            mysqli_query($conn, $sql);
+            mysqli_query($conn, $sql);  
             if ( mysqli_affected_rows($conn)>0){
                 echo "Records inserted successfully.";
             }else{
@@ -65,50 +83,65 @@
         }else{
             echo "Please fill in all fields.";
         }
-
-
+        
         }
-
-
-
-
+    
     ?>
-		<div class="dashboard-container">
-		  <div class="header-container">
-		  <div class="logo-container">
-		    <div class="logo">
-		      <h1 class="logo-text">Expert<span class="trademark">&reg;</span></h1>
-		    </div>
-		      <?php include 'adminNav.php';?>
-		  </div>
+    <div class= "training-form">
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
-
-        <p><label for="fileToUpload">Image: </label>
+        
+        
+    <p class="file-container"><label for="fileToUpload">Image:</label>
         <input type="file" name="fileToUpload" id="fileToUpload"/></p>
-
         <p><label for="tName">Training Name: </label>
         <input type="text" name="tName" id="tName"/></p>
-
+        
         <p><label for="tCategory">Training Category: </label>
         <select name="tCategory" id="tCategory">
-            <option value="Segmentation">Segmentation Workshop</option>
-            <option value="Co-creation">Co-creation Workshop</option>
-            <option value="Brainstorm">Brainstorm Workshop</option>
-            <option value="Activation">Team Activation Workshop</option>
+            <option value="segmentation">Segmentation Workshop</option>
+            <option value="co-creation">Co-creation Workshop</option>
+            <option value="brainstorm">Brainstorm Workshop</option>
+            <option value="teamActivation">Team Activation Workshop</option>
         </select></p>
-
         <p><label for="tLocation">Training Location: </label>
         <input type="text" name="tLocation" id="tLocation"/></p>
-
         <p><label for="tPrice">Training Price: </label>
         <input type="text" name="tPrice" id="tPrice"/></p>
-
         <p><label for="tDescription">Training Description: </label>
         <input type="text" name="tDescription" id="tDescription"/></p>
-
+        
+        
         <p><input type="submit" value="Add" name="submit"/></p>
     </form>
-	</div>
-	</div>
+    </div>
+
+    <footer>
+    <div class="footer-container">
+      <div class="footer-column">
+        <ul class="footer-nav">
+          <li><a href="#">Training</a></li>
+          <li><a href="#">Payment</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Account</a></li>
+        </ul>
+        <p class="footer-info">Phone: 123-456-7890 | Address: 123 Main St. | Email: info@expert.com</p>
+        <div class="footer-logo">
+
+          <p class="footer-copyright">Expert Training since 2023 &#169; </p>
+        </div>
+      </div>
+      <div class="footer-column">
+        <ul class="social-icons">
+          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+    
+      <script src="script/buttontop.js"></script>
 </body>
+</html>	
 </html>
