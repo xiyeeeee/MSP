c35ff163be7693532e7b2f2af6e4e1018d21fdca
01hey <103423772+01hey@users.noreply.github.com>
2023-04-22 22:08:14 +0800

    Update manage_request.php

--- MSPcode/manage_request.php
+++ MSPcode/manage_request.php
@@ -16,5 +16,64 @@
   </div>
   </div>
   </div>
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "expert_db";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  if (isset($_POST['accept'])) {
+    $sql = "UPDATE requests SET status='accepted' WHERE name='" . $_POST['name'] . "'";
+    mysqli_query($conn, $sql);
+  } elseif (isset($_POST['deny'])) {
+    $sql = "UPDATE requests SET status='denied' WHERE name='" . $_POST['name'] . "'";
+    mysqli_query($conn, $sql);
+  }
+
+  $sql = "SELECT name, training, tLocation, remark, status FROM requests";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Name</th>";
+    echo "<th>Training</th>";
+    echo "<th>Location</th>";
+    echo "<th>Remark</th>";
+    echo "<th>Status</th>";
+    echo "<th>Action</th>";
+    echo "</tr>";
+    while ($row = mysqli_fetch_assoc($result)) {
+      echo "<tr>";
+      echo "<td>" . $row["name"] . "</td>";
+      echo "<td>" . $row["training"] . "</td>";
+      echo "<td>" . $row["tLocation"] . "</td>";
+      echo "<td>" . $row["remark"] . "</td>";
+      echo "<td>" . $row["status"] . "</td>";
+      echo "<td>";
+      echo "<form method='post' action=''>";
+      echo "<input type='hidden' name='name' value='" . $row["name"] . "'>";
+      echo "<button type='submit' name='accept'>Accept</button>";
+      echo "<button type='submit' name='deny'>Deny</button>";
+      echo "</form>";
+      echo "</td>";
+      echo "</tr>";
+    }
+    echo "</table>";
+  } else {
+    echo "No requests";
+  }
+
+  mysqli_close($conn);
+  ?>
+
 </body>
 </html>
