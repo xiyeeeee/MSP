a3ff0bcd27d70b14bd7c6d82accf6d0a6b697a51
xiyeeeee <claudiachenxiye@gmail.com>
2023-04-25 11:14:32 +0800

    manage_request front end

--- MSPcode/manage_request.php
+++ MSPcode/manage_request.php
@@ -1,11 +1,11 @@
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/style3.css">
 	<script src="script.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 	<title>REQUEST: ETMP</title>
 </head>
-<body>
+<body id="manageRequestbg">
 <div class= "dashboard-container">
   <div class="header-container">
   <div class="logo-container">
@@ -80,6 +80,32 @@
 
   mysqli_close($conn);
   ?>
+
+<footer>
+    <div class="footer-container">
+      <div class="footer-column">
+        <ul class="footer-nav">
+          <li><a href="#">Training</a></li>
+          <li><a href="#">Payment</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Account</a></li>
+        </ul>
+        <p class="footer-info">Phone: 123-456-7890 | Address: 123 Main St. | Email: info@expert.com</p>
+        <div class="footer-logo">
+
+          <p class="footer-copyright">Expert Training since 2023 &#169; </p>
+        </div>
+      </div>
+      <div class="footer-column">
+        <ul class="social-icons">
+          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
 
 </body>
 </html>
